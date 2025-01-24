121b06707bcd34c6daa1b8682d60f2fd8e636c79
YanaHranovska1 <yanahranovska@gmail.com>
2025-01-24 07:15:25 +0200

    added register.html template now RegisterView returns register.html

--- hola_espanol/main/views.py
+++ hola_espanol/main/views.py
@@ -1,3 +1,5 @@
+import os
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
@@ -5,4 +7,6 @@ from django.views import View
 class RegisterView(View):
 
     def get(self, request):
-        return HttpResponse("Register page")
+        template_path = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', 'templates', 'register.html'))    # this expression gets absolute path to register.html file
+        with open(template_path, 'r') as f:
+            return HttpResponse(f.read())
