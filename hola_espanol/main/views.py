851d6475785d33d4921d22fd92f63995f5618006
Pitsan-Viktoriia <vikapitsan4@gmail.com>
2025-01-25 18:03:58 +0200

    Implemented user creation (by RegisterView)

--- hola_espanol/main/views.py
+++ hola_espanol/main/views.py
@@ -3,6 +3,9 @@ import os
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from django.db.utils import IntegrityError
+
+from .models import CustomUser
 
 class RegisterView(View):
 
@@ -10,3 +13,12 @@ class RegisterView(View):
         template_path = os.path.abspath(os.path.join(os.path.abspath(__file__), '..', 'templates', 'register.html'))    # this expression gets absolute path to register.html file
         with open(template_path, 'r') as f:
             return HttpResponse(f.read())
+
+    def post(self, request):
+        if (request.POST.get('password1') != request.POST.get('password2')):
+            return HttpResponse('Passwords do not match!')
+        try:
+            CustomUser.objects.create_user(username=request.POST.get('username'), password=request.POST.get('password1'))
+            return HttpResponse('User has been successfully created!')
+        except IntegrityError:      # this exception occurs when we try to create_user with username that already exists
+            return HttpResponse('User with this username already exists!')
