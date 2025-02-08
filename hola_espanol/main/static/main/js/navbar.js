95f39b0c6c348411103500bb3a78a39bb0ed1a22
Pitsan-Viktoriia <vikapitsan4@gmail.com>
2025-02-08 21:10:23 +0200

    Moved navbar id from <a> to <li>, because it caused trouble when hiding elements

--- hola_espanol/main/static/main/js/navbar.js
+++ hola_espanol/main/static/main/js/navbar.js
@@ -6,17 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
             throw new Error('request failed');
         }
     }).then(data => {
-        var username = data['username'];
         var message = data['message'];
         if (message != 'user not found')     // if user logged in
         {
-            console.log(data);
             document.getElementById('nav__login').style.display = 'none';
             document.getElementById('nav__register').style.display = 'none';
         }
         else 
         {
             document.getElementById('nav__profile').style.display = 'none';
+            document.getElementById('nav__learning').style.display = 'none';
         }
     }).catch(error => {
         console.error(error);
