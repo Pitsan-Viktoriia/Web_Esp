7bb6fbc8dc904371eb16328fb8a13e19871ddf91
YanaHranovska1 <yanahranovska@gmail.com>
2025-05-25 17:34:51 +0300

    now exercises are loaded in correct order (fixed async)

--- hola_espanol/main/static/main/js/grammar.js
+++ hola_espanol/main/static/main/js/grammar.js
@@ -4,59 +4,61 @@ async function getJsonByURL(url) {
     return data;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    getJsonByURL('/info/topic/' + data['id_list'][i] + '/').then(data => {
-        if (data['type'] == 'grammar') {
-            console.log(data)
-            // form section by creating elements
-            let section = document.createElement('section')
-            let topic_name = document.createElement('h1')
-            let materialsmaterials_h3 = document.createElement('h3')
-            let exercisesexercises_h3 = document.createElement('h3')
-            materials.innerText
-            let materials_ul = document.createElement('ul')
-            let exercises_ul = document.createElement('ul')
+document.addEventListener('DOMContentLoaded', async function() {
+    getJsonByURL('/info/topic/all/').then(async data => {
+        for (i = 0; i < data['id_list'].length; i++)
+        {
+            await getJsonByURL('/info/topic/' + data['id_list'][i] + '/').then(async data => {
+                if (data['type'] == 'grammar') {
+                    console.log(data)
+                    // form section by creating elements
+                    let section = document.createElement('section')
+                    let topic_name = document.createElement('h1')
+                    let materials_h3 = document.createElement('h3')
+                    let exercises_h3 = document.createElement('h3')
+                    let materials_ul = document.createElement('ul')
+                    let exercises_ul = document.createElement('ul')
 
-            // setting up elements
-            materials_h3.innerText = 'Матеріали'
-            exercises.innerTextexercises_h3.innerText = 'Вправи'
-            topic_name.innerText = data['name']
-            section.appendChild(topic_name)
-            section.appendChild(materials)
-            section.appendChild(exercises)
-            getJsonByURL('/info/topic/' + data['id'] + '/materials/').then(data => {
-                for (j = 0; j < data['id_list'].length; j++)
-                {
-                    getJsonByURL('/info/material/' + data['id_list'][j]).then(data => {
-                        let li = document.createElement('li')
-                        let a = document.createElement('a')
-                        a.setAttribute('href', 'material/' + data['id'])
-                        a.innerText = data['content']['name']
-                        li.appendChild(a)
-                        materials_ul.appendChild(li)
+                    // setting up elements
+                    materials_h3.innerText = 'Матеріали'
+                    exercises_h3.innerText = 'Вправи'
+                    topic_name.innerText = data['name']
+                    section.appendChild(topic_name)
+                    await getJsonByURL('/info/topic/' + data['id'] + '/materials/').then(async data => {
+                        for (j = 0; j < data['id_list'].length; j++)
+                        {
+                            await getJsonByURL('/info/material/' + data['id_list'][j]).then(data => {
+                                let li = document.createElement('li')
+                                let a = document.createElement('a')
+                                a.setAttribute('href', 'material/' + data['id'])
+                                a.innerText = data['content']['name']
+                                li.appendChild(a)
+                                materials_ul.appendChild(li)
+                            })
+                        }
                     })
+                    await getJsonByURL('/info/topic/' + data['id'] + '/exercises/').then(async data => {
+                        for (j = 0; j < data['id_list'].length; j++)
+                        {
+                            await getJsonByURL('/info/exercise/' + data['id_list'][j]).then(data => {
+                                let li = document.createElement('li')
+                                let a = document.createElement('a')
+                                a.setAttribute('href', 'exercise/' + data['id'])
+                                a.innerText = data['content']['name']
+                                li.appendChild(a)
+                                exercises_ul.appendChild(li)
+                            })
+                        }
+                    })
+                    section.appendChild(materials_h3)
+                    section.appendChild(materials_ul)
+                    section.appendChild(exercises_h3)
+                    section.appendChild(exercises_ul)
+
+                    let main = document.getElementById('main')
+                    main.appendChild(section)
                 }
             })
-            getJsonByURL('/info/topic/' + data['id'] + '/exercises/').then(data => {
-                for (j = 0; j < data['id_list'].length; j++)
-                {
-                    getJsonByURL('/info/exercise/' + data['id_list'][j]).then(data => {
-                        let li = document.createElement('li')
-                        let a = document.createElement('a')
-                        a.setAttribute('href', 'exercise/' + data['id'])
-                        a.innerText = data['content']['name']
-                        li.appendChild(a)
-                        exercises_ul.appendChild(li)
-                    })
-                }
-            })
-            section.appendChild(materials_h3)
-            section.appendChild(materials_ul)
-            section.appendChild(exercises_h3)
-            section.appendChild(exercises_ul)
-
-            let main = document.getElementById('main')
-            main.appendChild(section)
         }
     });
 }, false);
